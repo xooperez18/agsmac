27bdf1a161ae37af8706d1e856c357d41024132f
Angel Andrade <v1ru5tx@users.noreply.github.com>
2019-02-14 23:23:50 -0600

    Se suben cambió en la manera en como se comportan los circulos (#2)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,8 @@
 /*
-	Radius by TEMPLATED
-	templated.co @templatedco
-	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+    Radius by TEMPLATED
+    templated.co @templatedco
+    Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
 */
-
 (function($) {
 
 	skel.breakpoints({
@@ -16,84 +15,115 @@
 
 	$(function() {
 
+		// Este arreglo tendra el registro de todos los divs emergentes
+		// que hayan sido clickeados.
+		// Aqui solo debe de albergar uno
+		// esto con el fin de llevar el control para cuando se clicke en el documento
+		var imagenesClick = [];
+
 		var	$window = $(window),
 			$body = $('body'),
 			$header = $('#header'),
 			$footer = $('#footer');
 
 		// Disable animations/transitions until the page has loaded.
-			$body.addClass('is-loading');
+		$body.addClass('is-loading');
 
-			$window.on('load', function() {
-				window.setTimeout(function() {
-					$body.removeClass('is-loading');
-				}, 100);
-			});
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-loading');
+			}, 100);
+		});
 
 		// Fix: Placeholder polyfill.
-			$('form').placeholder();
+		$('form').placeholder();
 
 		// Prioritize "important" elements on medium.
-			skel.on('+medium -medium', function() {
-				$.prioritize(
-					'.important\\28 medium\\29',
-					skel.breakpoint('medium').active
-				);
-			});
+		skel.on('+medium -medium', function() {
+			$.prioritize(
+				'.important\\28 medium\\29',
+				skel.breakpoint('medium').active
+			);
+		});
 
 		// Header.
-			$header.each( function() {
+		$header.each( function() {
 
-				var t 		= jQuery(this),
-					button 	= t.find('.button');
+			var t 		= jQuery(this),
+				button 	= t.find('.button');
 
-				button.click(function(e) {
+			button.click(function(e) {
 
-					t.toggleClass('hide');
+				t.toggleClass('hide');
 
-					if ( t.hasClass('preview') ) {
-						return true;
-					} else {
-						e.preventDefault();
+				if ( t.hasClass('preview') ) {
+					return true;
+				} else {
+					e.preventDefault();
+				}
+
+			});
+
+		});
+
+
+		$(document).click(function(e){
+			var claseIdentificadora = e.target.parentElement;
+				claseIdentificadora = claseIdentificadora.parentElement.className;
+				if(claseIdentificadora != "mostrarMas"){
+					var count = 0;
+					$('.mostrarInfoH').each(function(){
+						if( $(this).hasClass("show") )
+						{
+							$(this).removeClass("show");
+							$(this).addClass("hide");
+							count++;
+						}
+					});
+				}
+				if(count != 0) {
+					e.preventDefault();
+				}
+		});
+		//Efecto imágenes laterales
+		$('.mostrarInfoH').each( function() {
+			var t 		= jQuery(this);
+			var enlace 	= t.find('.mostrarMas');
+
+			enlace.click( function(){
+				// Eliminamos todos los que tengan esta cla
+				$('.mostrarInfoH').each(function(){
+					if( $(this).hasClass("show") )
+					{
+						$(this).removeClass("show");
+						$(this).addClass("hide");
 					}
-
 				});
+				t.toggleClass('hide');
+				t.toggleClass('show');
+				/*if ( t.hasClass('preview') ) {
+                    return true;
+                } else {
+                    e.preventDefault();
+                }*/
 
-			});
+			} );
+		} );
 
-//Efecto imágenes laterales
-$('.mostrarInfoH').each( function() {
-				var t 		= jQuery(this);
-					var enlace 	= t.find('.mostrarMas');
-
-				enlace.click(function(e) {
-
-					t.toggleClass('hide');
-					t.toggleClass('show');
-
-					/*if ( t.hasClass('preview') ) {
-						return true;
-					} else {
-						e.preventDefault();
-					}*/
-
-				});
-
-			});
 
 		// Footer.
-			$footer.each( function() {
+		$footer.each( function() {
 
-				var t 		= jQuery(this),
-					inner 	= t.find('.inner'),
-					button 	= t.find('.info');
+			var t 		= jQuery(this),
+				inner 	= t.find('.inner'),
+				button 	= t.find('.info');
 
-				button.click(function(e) {
-					t.toggleClass('show');
-					e.preventDefault();
-				});
-
+			button.click(function(e) {
+				t.toggleClass('show');
+				e.preventDefault();
 			});
+
+		});
 
 	});
 
