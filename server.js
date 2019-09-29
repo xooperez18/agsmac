e373b45d80b4d858bb67f0b968b07e3c9dc10cb9
Sergio Marquez de Silva <semarque@microsoft.com>
2019-09-28 18:18:41 -0700

    Add node server and google map

---
+++ server.js
@@ -0,0 +1,24 @@
+var connect = require('connect');
+var serveStatic = require('serve-static');
+connect().use(serveStatic(__dirname)).listen(8080, function(){
+    console.log('Server running on 8080...');
+});
+
+/*
+1.- Install connect and serve-static with NPM
+
+$ npm install connect serve-static
+
+2.- Create server.js file with this content:
+
+var connect = require('connect');
+var serveStatic = require('serve-static');
+connect().use(serveStatic(__dirname)).listen(8080, function(){
+    console.log('Server running on 8080...');
+});
+
+3.-Run with Node.js
+
+$ node server.js
+You can now go to http://localhost:8080
+*/
